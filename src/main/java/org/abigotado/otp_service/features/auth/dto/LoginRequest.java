ff726a4a46a9b1eea4ecb3bf432650f22845cb0b
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-04-09 15:13:17 -0300

    Merge pull request #2
    * add logging

--- src/main/java/org/abigotado/otp_service/features/auth/dto/LoginRequest.java
+++ src/main/java/org/abigotado/otp_service/features/auth/dto/LoginRequest.java
@@ -1,8 +1,10 @@
 package org.abigotado.otp_service.features.auth.dto;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString(exclude = "password")
 public class LoginRequest {
     private String username;
     private String password;
